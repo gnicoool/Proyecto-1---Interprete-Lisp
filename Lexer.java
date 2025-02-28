67102ea984a3317181ae4ea653ae0a003cf021cd
Nicolle Girón <jackelyngironvillacinda@gmail.com>
2025-02-28 11:24:45 -0600

    Creacion de clases Lexer, Parser y Main

---
+++ Lexer.java
@@ -0,0 +1,37 @@
+import java.util.List;
+
+public class Lexer {
+     private List<String> tokens;
+    private int index;
+    public Lexer(List<String> tokens) {
+        this.tokens = tokens;
+        this.index = 0;
+    }
+    public String next() {
+        return tokens.get(index++);
+    }
+    public String peek() {
+        return tokens.get(index);
+    }
+    public boolean hasNext() {
+        return index < tokens.size();
+    }
+    public void reset() {
+        index = 0;
+    }
+    public int getIndex() {
+        return index;
+    }
+    public void setIndex(int index) {
+        this.index = index;
+    }
+    public List<String> getTokens() {
+        return tokens;
+    }
+    public void setTokens(List<String> tokens) {
+        this.tokens = tokens;
+    }
+    public void addToken(String token) {
+        tokens.add(token);
+    }
+}
