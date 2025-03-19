838c4f3d6f6b1a27e153d3f07b96c9850936ea6b
stan-2021131 <sergiocoromac@gmail.com>
2025-03-19 07:48:43 -0600

    Se agregó la estructura de la main para el testeo de funciones lisp

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Main{
+    public static void main(String[] args) {
+        Lector l = new Lector();
+        ArrayList<ArrayList<String>> funcionesValidas = l.procesarArchivo();
+        Operador<String> o = new Operador<>();
+        // Imprimir los tokens de las funciones válidas
+        /* for (ArrayList<String> tokens : funcionesValidas) {
+            for (String token : tokens) {
+                System.out.println("  " + token);
+            }
+            System.out.println();
+        } */
+        System.out.println(o.operar(funcionesValidas.get(0)));
+        System.out.println(o.operar(funcionesValidas.get(1)));
+    }
+}
