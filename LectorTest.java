ec3af2b7c6e1cad0cede68160216af46fd8ae077
unknown <alejandraviles2005@gmail.com>
2025-03-20 22:53:57 -0600

    Se agregaron las pruebas JUnit necesarias para el código con unas mejoras.

--- LectorTest.java
+++ LectorTest.java
@@ -1,8 +1,6 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
+import org.junit.Test; 
+import static org.junit.Assert.*; 
+import java.util.ArrayList; 
 
 public class LectorTest {
     Lector l = new Lector();
@@ -32,5 +30,27 @@ public class LectorTest {
         assertEquals("+", tokens.get(2));
         assertEquals("2", tokens.get(3));
         assertEquals("3", tokens.get(4));
+    }
+
+    @Test
+    public void testSintaxisCond() {
+        String text = "(COND (> 1 2) (EQUAL 1 1))";
+        ArrayList<String> funciones = l.sintaxis(text);
+        assertEquals(1, funciones.size());
+        assertTrue(funciones.get(0).contains("COND"));
+    }
+
+    @Test
+    public void testTokenRegexCond() {
+        ArrayList<String> tokens = l.tokenRegex("(COND (> 1 0) (+ 1 2))");
+        assertEquals(7, tokens.size());
+
+        assertEquals("COND", tokens.get(0));
+        assertEquals(">", tokens.get(1));
+        assertEquals("1", tokens.get(2));
+        assertEquals("0", tokens.get(3));
+        assertEquals("+", tokens.get(4));
+        assertEquals("1", tokens.get(5));
+        assertEquals("2", tokens.get(6));
     }
 }
