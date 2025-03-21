9dd2483fbb42aecaddb37f32fe18c3131d3a5b82
stan-2021131 <sergiocoromac@gmail.com>
2025-03-20 19:14:39 -0600

    Se agregaron comentarios

--- Predicados.java
+++ Predicados.java
@@ -1,9 +1,23 @@
-
+/**
+ * Clase predicados para la evaluacion de condicionales simples de lisp
+ * 
+ * Acepta valores genericos, pero que sean comparables, como numeros
+ * 
+ * @author Sergio Tan
+ */
 import java.util.List;
 
 public class Predicados<T>{
-    //Predicados
-    //Mayor y menor que
+    /**
+     * Recibe 2 valores y en base a predicado elegido realizará la condición
+     * 
+     * @param predicado Caso a evaluar, puede ser < o >
+     * @param val1 Primer valor a evaluar
+     * @param val2 Segundo valor a evaluar
+     * @return valor verdadero o falso en función a la condición
+     * @throws NumberFormatException en caso de que los valores recibidos no sean numericos
+     * @throws RuntimeException en caso de que predicado no exista
+     */
     public boolean MAYOR_MENOR_QUE(String predicado, T val1, T val2){
         double value1 = 0.00;
         double value2 = 0.00;
@@ -20,17 +34,35 @@ public class Predicados<T>{
         }
     }
 
-    //Convierte los datos a un String para compararlos con un EQUALS
+    /**
+     * Evalua si dos expresiones son iguales al convertirlas a cadenas de texto y hacer un equals
+     * 
+     * @param val1 Primer valor a evaluar
+     * @param val2 Segundo valor a evaluar
+     * @return Verdadero si son iguales, falso en caso contrario
+     */
     public boolean EQUAL(T val1, T val2){
         String val1Str = val1.toString();
         String val2Str = val2.toString();
         return val1Str.equals(val2Str);
     }
 
+    /**
+     * Evalua si lo recibido es un dato unico o un listado de ellos
+     * 
+     * @param data listado de datos en los que se basa para la evaluación
+     * @return verdadero si el tamaño del listado es 1; falso en caso contrario
+     */
     public boolean ATOM(List<T> data){
         return data.size()==1;
     }
 
+    /**
+     * Evalua si lo recibido es un listado de valores
+     * 
+     * @param data listado de datos en los que se basa para la evaluación
+     * @return verdadero si el tamaño del listado es distinto a 1 y 0; falso en caso contrario
+     */
     public boolean LIST(List<T> data){
         return data.size()!=1 && !data.isEmpty();
     }
