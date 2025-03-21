7e8493729616c87bb3d6c33feba4819c5cbfd3bc
unknown <alejandraviles2005@gmail.com>
2025-03-20 20:16:00 -0600

    Se trabajó en la añadidura de prints para monitorear los resultados que se obtenían al hacer pruebas de QUOTE y COND.

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main{
         Lector l = new Lector();
         ArrayList<ArrayList<String>> funcionesValidas = l.procesarArchivo();
         Operador<String> o = new Operador<>();
+
+        //System.out.println(Condicionales);
         // Imprimir los tokens de las funciones válidas
         /* for (ArrayList<String> tokens : funcionesValidas) {
             for (String token : tokens) {
@@ -14,5 +16,18 @@ public class Main{
         } */
         System.out.println(o.operar(funcionesValidas.get(0)));
         System.out.println(o.operar(funcionesValidas.get(1)));
+        System.out.println(o.operar(funcionesValidas.get(2)));
+        System.out.println(o.operar(funcionesValidas.get(3)));
+        /*System.out.println(o.operar(funcionesValidas.get(4)));
+        System.out.println(o.operar(funcionesValidas.get(5)));
+        System.out.println(o.operar(funcionesValidas.get(6)));
+        System.out.println(o.operar(funcionesValidas.get(7)));
+        System.out.println(o.operar(funcionesValidas.get(8)));
+        System.out.println(o.operar(funcionesValidas.get(9)));
+        System.out.println(o.operar(funcionesValidas.get(10)));
+        System.out.println(o.operar(funcionesValidas.get(11)));
+        System.out.println(o.operar(funcionesValidas.get(12)));
+        System.out.println(o.operar(funcionesValidas.get(13)));
+        System.out.println(o.operar(funcionesValidas.get(14)));*/
     }
 }
